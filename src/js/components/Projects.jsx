c25c10d9f4870b91cf807b46eff3a27b187fe258
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-04-24 23:43:57 +0000

    project page update

--- src/js/components/Projects.jsx
+++ src/js/components/Projects.jsx
@@ -3,20 +3,26 @@ import React from 'react';
 
 const Projects = () => {
     return(
-        <div>
+        <div className="projects-section">
             <h1 className="projects-tittle">My Projects</h1>
             <div className="projects-container">
                 <div className="card-project">
                     <img className="project-image" src="https://media.geeksforgeeks.org/wp-content/uploads/20240409180552/All-Phases.webp"/>
                     <h3>Project 1</h3>
-                    
                 </div>
                 <div className="card-project">
-                    <h3>Project 2</h3>
+                    <img className="project-image" src="https://media.geeksforgeeks.org/wp-content/uploads/20240409180552/All-Phases.webp"/>
+                    <h3>Project 1</h3>
                 </div>
                 <div className="card-project">
-                    <h3>Project 3</h3>
+                    <img className="project-image" src="https://media.geeksforgeeks.org/wp-content/uploads/20240409180552/All-Phases.webp"/>
+                    <h3>Project 1</h3>
                 </div>
+                <div className="card-project">
+                    <img className="project-image" src="https://media.geeksforgeeks.org/wp-content/uploads/20240409180552/All-Phases.webp"/>
+                    <h3>Project 1</h3>
+                </div>
+               
             </div>
         </div>
     )
