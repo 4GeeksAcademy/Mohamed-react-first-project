b1f7d354021d388d50b310ad060d3fea833288f5
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-04-26 18:02:44 +0000

    style adjust

--- src/js/components/Projects.jsx
+++ src/js/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = ({image , title , projectRepo}) => {
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     
-    <a href={projectRepo} className="btn btn-primary">Project Repository</a>
+    <a href={projectRepo} className="btn ">Project Repository</a>
   </div>
 </div>
 
