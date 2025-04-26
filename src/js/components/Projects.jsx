d0d97fc96d220f8c3d2f9a404fcc6827d16a543a
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-04-26 00:11:45 +0000

    new features

--- src/js/components/Projects.jsx
+++ src/js/components/Projects.jsx
@@ -9,12 +9,8 @@ const Projects = () => {
   <img src={card} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Random Card Generator</h5>
-    <div class="hstack gap-3">
-  <div class=" p-2">HTML</div>
-  <div class="p-2">CSS</div>
-  <div class="p-2">JS</div>
-</div>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    
+    <a href="#" className="btn btn-primary">Project Repository</a>
   </div>
 </div>
     )
